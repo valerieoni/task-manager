0fab20bae468bde8977c3fc9035026b9142f6fb4
valerieoni <valeriewil@yahoo.com>
2022-10-02 18:29:06 +0000

    add update and delete crud functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -8,7 +8,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -19,3 +20,21 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+});
