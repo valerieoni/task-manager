43f5724b6902fc872af4abeffe6d4d4e0faa64fb
valerieoni <valeriewil@yahoo.com>
2022-10-02 18:52:02 +0000

    add task implementation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     //sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-document.addEventListener('DOMContentLoaded', function() {
-  let modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-});
+
+    // Modal
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+  });
